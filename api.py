2ce39987a2c91d9574e0bdcd570a2c05bf752ece
zhangupkai <zhangshangkai@foxmail.com>
2021-04-08 20:49:44 +0800

    api逻辑修正

--- api.py
+++ api.py
@@ -3,9 +3,9 @@ from flask import Flask, request, jsonify
 import joblib
 import logging
 import sys
+import os
 
 app = Flask(__name__)
-model = joblib.load('model/cluster.pkl')
 
 
 @app.route('/')
@@ -15,15 +15,11 @@ def home():
     return 'Hello World'
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    print('Start!')
+@app.route('/collect', methods=['POST'])
+def collect():
+    print('collect')
     data = request.get_json()
-    print('data', data)
-    # duration = 0.0
-    # for v in data.values():
-    #     duration = v
-
+    print(data)
     # 写入data.txt，追加模式'a'
     file = open('data/data.txt', 'a')
     file.writelines(['\n', str(data['duration'])])
@@ -40,7 +36,44 @@ def predict():
                      str(data['pressureAvg'])])
     file.close()
 
-    prediction = model.predict(np.mat([[data['duration'], 1]]))
+    model = joblib.load('model/cluster.pkl')
+    prediction = model.predict(np.mat([[data['duration'], data['sizeAvg']]]))
+    output = {
+        'result': int(prediction[0])
+    }
+    return jsonify(output)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    print('predict')
+    data = request.get_json()
+    print('data', data)
+    # duration = 0.0
+    # for v in data.values():
+    #     duration = v
+
+    # # 写入data.txt，追加模式'a'
+    # file = open('data/data.txt', 'a')
+    # file.writelines(['\n', str(data['duration'])])
+    # file.close()
+    #
+    # # 写入collect.txt
+    # file = open('data/collect.txt', 'a')
+    # file.writelines(['\n', str(data['duration']), ',',
+    #                  str(data['sizeAtDown']), ',',
+    #                  str(data['sizeAtUp']), ',',
+    #                  str(data['sizeAvg']), ',',
+    #                  str(data['pressureAtDown']), ',',
+    #                  str(data['pressureAtUp']), ',',
+    #                  str(data['pressureAvg'])])
+    # file.close()
+
+    # 0 成功，1 失败
+    res = os.system('python cluster.py')
+
+    model = joblib.load('model/cluster.pkl')
+    prediction = model.predict(np.mat([[data['duration'], data['sizeAvg']]]))
     output = {
         'result': int(prediction[0])
     }
